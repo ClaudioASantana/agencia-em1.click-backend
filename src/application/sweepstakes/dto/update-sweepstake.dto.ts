e20234029cfb8b816991f8b8f31dbfe4a627c1f1
CLAUDIO AMORIM DE SANTANA <claudio.a.santana@outlook.com>
2026-03-30 09:27:50 -0300

    api de prod.

--- src/application/sweepstakes/dto/update-sweepstake.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSweepstakeDto } from './create-sweepstake.dto';
-import { IsBoolean, IsOptional } from 'class-validator';
-
-export class UpdateSweepstakeDto extends PartialType(CreateSweepstakeDto) {
-  @IsBoolean()
-  @IsOptional()
-  active?: boolean;
-}
