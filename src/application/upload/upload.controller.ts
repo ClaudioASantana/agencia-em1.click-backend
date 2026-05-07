c4419fee77d0aacd44609ebbdbef37cb5a1898d3
Claudio Amorim <claudiosantana_otg@cnc.com.br>
2026-05-07 09:36:42 -0300

    remover arquivos de suporta da raiz do projeto.

--- src/application/upload/upload.controller.ts
+++ src/application/upload/upload.controller.ts
@@ -49,8 +49,11 @@ export class UploadController {
     @UploadedFile(
       new ParseFilePipe({
         validators: [
-          new MaxFileSizeValidator({ maxSize: 5 * 1024 * 1024 }), // 5MB
-          new FileTypeValidator({ fileType: /image\/(jpeg|png|webp|gif)/ }),
+          new MaxFileSizeValidator({ maxSize: 15 * 1024 * 1024 }), // 15MB
+          new FileTypeValidator({
+            fileType: /image\/(jpeg|jpg|png|webp|gif)/,
+            skipMagicNumbersValidation: true,
+          }),
         ],
       }),
     )
